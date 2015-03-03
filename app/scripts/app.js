4d644bbab75ac357b4336353e11c87419d9c0538
Diego d'Ursel <diegodurs@gmail.com>
2015-03-03 18:21:16 +0100

    favorites tabs working
    I needed to remove the <template> tag because everything inside it is
    shadow DOM I believe, so cannot (or should not) be accessed from outside

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,10 @@
   'use strict';
 
   var tabs = document.querySelector('paper-tabs');
+  var list = document.querySelector('post-list');
+
   tabs.addEventListener('core-select', function() {
-    console.log("Selected: " + tabs.selected);
+    list.show = tabs.selected;
   });
 
 // wrap document so it plays nice with other libraries
